36d423ec69b9b56d6cb0ff5e4d4c37c6b228ad93
grace-rono <ronograce04@gmail.com>
2023-03-17 06:18:57 +0000

    Write a C program that prints exactly "Programming is like building a multilingual puzzle

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,9 @@
-# include <iostream>
+# include <stdio.h>
 /**
   * C program that prints exactly
    *"Programming is like building a multilingual puzzle
+   * main -entry point
+   *Return Always 0 {success}
  */
 
 /*main function*/
