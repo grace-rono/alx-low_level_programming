7ae458762055bcec766206a1d5190f7759898d64
grace-rono <ronograce04@gmail.com>
2023-03-30 07:48:53 -0700

    function that concatenates strings

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -17,13 +17,13 @@ char *_strncat(char *dest, char *src, int n)
 	{
 		i++;
 	}
-			
+
 	while (j < n && src[j] != '\0')
-		{
-			dest[i] = src[j];
-			i++;
-			j++;
-		}
+	{
+		dest[i] = src[j];
+		i++;
+		j++;
+	}
 	dest[i] = '\0';
 	return (dest);
 }
