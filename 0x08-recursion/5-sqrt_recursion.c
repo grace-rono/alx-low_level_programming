b591bec9bbfa89b916045814ed25ec03a97d8d41
grace-rono <ronograce04@gmail.com>
2023-04-28 05:28:41 -0700

    deleted files

--- 0x08-recursion/5-sqrt_recursion.c
+++
@@ -1,9 +0,0 @@
-#include "main.h"
-/**
- * _sqrt_recursion -  function that returns the natural square root of a number.
- * n - the value
- * Return: returns square root
- */
-int _sqrt_recursion(int n)
-{
-	
--- 0x0E-structures_typedef/4-new_dog.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-#include "dog.h"
-
-dog_t *new_dog(char *name, float age, char *owner)
-{
-	 
-}
